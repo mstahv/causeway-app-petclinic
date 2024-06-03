771630c4fb125a1ef0c1ce4190e7c8dff01bb49b
Matti Tahvonen <matti@vaadin.com>
2024-06-03 15:40:43 +0300

    Public Vaadin UI to check appointments

---
+++ webapp/src/main/java/domainapp/webapp/custom/AppointmentCheckerView.java
@@ -0,0 +1,51 @@
+package domainapp.webapp.custom;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import domainapp.modules.visit.dom.visit.Visit;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.List;
+
+@Route
+public class AppointmentCheckerView extends VerticalLayout {
+    TextField name  = new TextField("Your name");
+    TextField pet  = new TextField("Pet name");
+    Button search = new Button("Search");
+    public Div searchResults = new Div();
+
+    public AppointmentCheckerView(PublicAppointmentService publicAppointmentService) {
+        add(new H1("Check your appointment"));
+        add(new Paragraph("Forgot your appointment time? No worries, as long as you still remember your own name and your pet's name, check out your appointments below."));
+        var searchForm = new HorizontalLayout(name, pet, search);
+        searchForm.setAlignItems(Alignment.END);
+        add(searchForm);
+        add(searchResults);
+
+        search.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        search.addClickShortcut(Key.ENTER);
+
+        search.addClickListener(e -> {
+            searchResults.removeAll();
+            List<Visit> visits = publicAppointmentService.findByOwnerAndPet(name.getValue(), pet.getValue());
+            if(visits.isEmpty()) {
+                searchResults.add(new H3("No appointments found, check your search terms all call customer service!"));
+            } else {
+                searchResults.add(new H3("Following appointment times for " + pet.getValue() + " found:"));
+                visits.forEach(v -> {
+                    searchResults.add(new Paragraph(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).format(v.getVisitAt())));
+                });
+            }
+        });
+    }
+}
