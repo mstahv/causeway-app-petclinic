154dd3a5d3e2f483686d500cd3e80ca346e98bfb
Matti Tahvonen <matti@vaadin.com>
2024-06-04 12:39:01 +0300

    Custom Vaadin view and a service layer exposing certain API for the custom UI

---
+++ webapp/src/main/java/domainapp/webapp/custom/PublicAppointmentService.java
@@ -0,0 +1,56 @@
+package domainapp.webapp.custom;
+
+
+import domainapp.modules.petowner.dom.petowner.PetOwner;
+import domainapp.modules.petowner.dom.petowner.PetOwners;
+import domainapp.modules.visit.contributions.PetOwner_visits;
+import domainapp.modules.visit.dom.visit.Visit;
+import jakarta.inject.Inject;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import lombok.RequiredArgsConstructor;
+import org.apache.causeway.applib.services.iactnlayer.InteractionContext;
+import org.apache.causeway.applib.services.iactnlayer.InteractionService;
+import org.apache.causeway.applib.services.user.UserMemento;
+import org.apache.causeway.applib.services.wrapper.WrapperFactory;
+import org.apache.causeway.applib.services.xactn.TransactionalProcessor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor(onConstructor_ = {
+   @Inject})
+public class PublicAppointmentService {
+
+
+   private final InteractionService interactionService;
+   private final TransactionalProcessor transactionalProcessor;
+   private final WrapperFactory wrapperFactory;
+   private final PetOwners petOwners;
+  
+   public List<Visit> findByOwnerAndPet(String ownerName, String petName) {
+       Optional<List<Visit>> visits = callAsSven(() -> {
+           PetOwner petOwner = petOwners.findByNameExact(ownerName);
+           if(petOwner != null) {
+               PetOwner_visits petOwnerVisits = wrapperFactory.wrapMixin(PetOwner_visits.class, petOwner);
+               return petOwnerVisits.coll().stream()
+                       .filter(v -> v.getPet().getName().equals(petName))
+                       .toList();
+           }
+           return null;
+       });
+       return visits.orElse(Collections.emptyList());
+   }
+
+
+   private <T> Optional<T> callAsSven(final Callable<T> callable) {
+       return interactionService.call(
+               InteractionContext.ofUserWithSystemDefaults(UserMemento.ofName("sven")),
+               () -> transactionalProcessor.callWithinCurrentTransactionElseCreateNew(callable)
+       )
+               .ifFailureFail()
+               .getValue();
+   }
+}
